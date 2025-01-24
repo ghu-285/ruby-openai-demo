be47acfdfeb043ea3d8a276d7ccf6b67635a39a1
ghu-285 <151415214+ghu-285@users.noreply.github.com>
2025-01-24 17:31:32 +0000

    chat

--- chat.rb
+++ chat.rb
@@ -1,1 +1,30 @@
 # Write your solution here!
+require "openai"
+require "dotenv/load"
+
+client = OpenAI::Client.new(access_token: ENV.fetch("OPENAI_API_KEY"))
+
+line = ""
+
+50.times do
+  line += "-"
+end
+
+request = ""
+message_list = []
+
+while request != "bye"
+  pp line
+  pp "Hello! How can I help you today?"
+  request = gets.chomp
+  message_list.push({"role" => "user", "content" => request})
+  api_response = client.chat(
+    parameters: {
+      model: "gpt-4",
+      messages: message_list
+    }
+  )
+  response = api_response["choices"][0]["message"]["content"]
+  message_list.push("role" => "system", "content" => response)
+  pp response
+end
